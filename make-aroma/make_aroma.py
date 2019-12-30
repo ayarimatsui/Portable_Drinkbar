832da4351e913e1c7165992374900e8358862b45
ayarimatsui <55052936+ayarimatsui@users.noreply.github.com>
2019-12-30 23:52:21 +0900

    modified make_aroma.py

--- make-aroma/make_aroma.py
+++ make-aroma/make_aroma.py
@@ -15,72 +15,72 @@ class MakeAroma():
 
         # GPIO初期化
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(fan1_pin, GPIO.OUT)
-        GPIO.setup(fan2_pin, GPIO.OUT)
-        GPIO.setup(fan3_pin, GPIO.OUT)
-        GPIO.setup(fan4_pin, GPIO.OUT)
-        GPIO.setup(fan5_pin, GPIO.OUT)
-        GPIO.setup(fan6_pin, GPIO.OUT)
+        GPIO.setup(self.fan1_pin, GPIO.OUT)
+        GPIO.setup(self.fan2_pin, GPIO.OUT)
+        GPIO.setup(self.fan3_pin, GPIO.OUT)
+        GPIO.setup(self.fan4_pin, GPIO.OUT)
+        GPIO.setup(self.fan5_pin, GPIO.OUT)
+        GPIO.setup(self.fan6_pin, GPIO.OUT)
 
 
     def fan_alloff(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan1_on(self):
-        GPIO.output(fan1_pin,GPIO.HIGH)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.HIGH)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan2_on(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.HIGH)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.HIGH)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan3_on(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.HIGH)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.HIGH)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan4_on(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.HIGH)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.HIGH)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan5_on(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.HIGH)
-        GPIO.output(fan6_pin,GPIO.LOW)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.HIGH)
+        GPIO.output(self.fan6_pin,GPIO.LOW)
 
 
     def fan6_on(self):
-        GPIO.output(fan1_pin,GPIO.LOW)
-        GPIO.output(fan2_pin,GPIO.LOW)
-        GPIO.output(fan3_pin,GPIO.LOW)
-        GPIO.output(fan4_pin,GPIO.LOW)
-        GPIO.output(fan5_pin,GPIO.LOW)
-        GPIO.output(fan6_pin,GPIO.HIGH)
+        GPIO.output(self.fan1_pin,GPIO.LOW)
+        GPIO.output(self.fan2_pin,GPIO.LOW)
+        GPIO.output(self.fan3_pin,GPIO.LOW)
+        GPIO.output(self.fan4_pin,GPIO.LOW)
+        GPIO.output(self.fan5_pin,GPIO.LOW)
+        GPIO.output(self.fan6_pin,GPIO.HIGH)
